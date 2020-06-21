109ac287cb253d61c6b19d62e08d4972640fac70
mannvender <mannvender001@gmail.com>
2020-06-21 07:51:35 +0530

    replacing hashrouter and homepage name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // library imports
 import React from "react"
-import { HashRouter as Router, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
 import "App.css"
