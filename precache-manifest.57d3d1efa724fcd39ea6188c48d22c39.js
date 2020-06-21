258dd238642590e705799bf58205591f210977b5
mannvender <mannvender001@gmail.com>
2020-06-21 07:35:54 +0530

    Updates

--- precache-manifest.57d3d1efa724fcd39ea6188c48d22c39.js
+++ precache-manifest.57d3d1efa724fcd39ea6188c48d22c39.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3580dcf209809a7e8e0cd724c9b22d6b",
+    "revision": "44416f8b6e624dc6a1b2017dcebd71a1",
     "url": "/praracayitr-parichay/index.html"
   },
   {
-    "revision": "f063370a918b6facde7f",
+    "revision": "68576ff05288c8930f94",
     "url": "/praracayitr-parichay/static/css/main.9d7d9642.chunk.css"
   },
   {
-    "revision": "071c4d091c94facc3399",
-    "url": "/praracayitr-parichay/static/js/2.522736b4.chunk.js"
+    "revision": "51e83697bd148ba0ada5",
+    "url": "/praracayitr-parichay/static/js/2.be72236b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/praracayitr-parichay/static/js/2.522736b4.chunk.js.LICENSE.txt"
+    "url": "/praracayitr-parichay/static/js/2.be72236b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f063370a918b6facde7f",
-    "url": "/praracayitr-parichay/static/js/main.070fbb76.chunk.js"
+    "revision": "68576ff05288c8930f94",
+    "url": "/praracayitr-parichay/static/js/main.eb971cc5.chunk.js"
   },
   {
     "revision": "678a327e6838cf4da4c7",
